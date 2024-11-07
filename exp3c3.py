65c7c9844bfa03810b4396192661840ae30acefb
Fathimamufeedak <24mcag09@meaec.edu.in>
2024-11-07 15:49:46 +0530

    Create exp3c3.py

---
+++ exp3c3.py
@@ -0,0 +1,6 @@
+my_list=list(map(int,input("enter the elements").split()))
+total=0
+for i in range(0,len(my_list)):
+    total=total+my_list[i]
+print("sum of all elements in given list:",total)    
+    
