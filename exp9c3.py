bc38e90d990b34e2e4490dac9b853dc087890b04
Fathimamufeedak <24mcag09@meaec.edu.in>
2024-11-07 15:55:24 +0530

    Create exp9c3.py

---
+++ exp9c3.py
@@ -0,0 +1,13 @@
+num=int(input("enter a number:"))
+original_num=num
+num_digits=len(str(num))
+sum_of_powers=0
+while num>0:
+    digit=num%10
+    sum_of_powers+=digit**num_digits
+    num=num//10
+if sum_of_powers==original_num:
+    print(f"{original_num} is an armstrong number")
+else:
+    print(f"{original_num} is not an armstrong number")
+~                                                          
